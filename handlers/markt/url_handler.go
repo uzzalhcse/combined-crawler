a61c76e3dbd28f9f3a3215c7518f41e0906c0c11
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-06-21 22:20:29 +0600

    Refactor handleProducts to simplify item processing and improve error handling in url_handler.go

--- handlers/markt/url_handler.go
+++ handlers/markt/url_handler.go
@@ -3,7 +3,6 @@ package markt
 import (
 	"combined-crawler/constant"
 	"combined-crawler/pkg/ninjacrawler"
-	"fmt"
 	"github.com/playwright-community/playwright-go"
 	"time"
 )
@@ -21,65 +20,45 @@ func UrlHandler(crawler *ninjacrawler.Crawler) {
 func handleProducts(ctx ninjacrawler.CrawlerContext) []ninjacrawler.UrlCollection {
 	var urls []ninjacrawler.UrlCollection
 	productLinkSelector := ".p-product-detail__review a.c-text-link.u-color-text--link.c-text-link--underline"
-	clickAndWaitButton(ctx.App, ".u-hidden-sp li button", ctx.Page)
-	const maxRetries = 1
-	const retryInterval = 10 * time.Second
+	//clickAndWaitButton(ctx.App, ".u-hidden-sp li button", ctx.Page)
+
 	items, err := ctx.Page.Locator("ul.p-card-list-no-scroll li.p-product-card.p-product-card--large").All()
 	if err != nil {
-		ctx.App.Logger.Info("Error fetching items:", err)
+		ctx.App.Logger.Warn("Error fetching items:", err)
 		return urls
 	}
 
 	for i, item := range items {
-		err = retryWithSleep(maxRetries, retryInterval, func(attempt int) error {
-			if attempt > 1 {
-				ctx.App.Logger.Info("Attempt #%d, waiting %v before retrying item index %d of %d & URL %s", attempt, retryInterval, i, len(items), ctx.UrlCollection.Url)
-			}
-			err := item.Click()
-			if err != nil {
-				ctx.App.Logger.Warn("Failed to click on Product Card: %v", err)
-				return err
-			}
+		if err := item.Click(); err != nil {
+			ctx.App.Logger.Warn("Failed to click on Product Card: %v", err)
+			continue
+		}
 
-			// Wait for the modal to open and the link to be available
-			_, err = ctx.Page.WaitForSelector(productLinkSelector)
-			if err != nil {
-				closeModal(ctx)
-				ctx.App.Logger.Warn("Failed To Open Modal, item index %d of %d %s %v", i, len(items), ctx.UrlCollection.Url, err.Error())
-				return err
-			}
-
-			doc, err := ctx.App.GetPageDom(ctx.Page)
-			if err != nil {
-				ctx.App.Logger.Warn("Error getting page DOM:", err)
-				return err
-			}
-
-			productLink, exist := doc.Find(productLinkSelector).First().Attr("href")
-			fullUrl := ctx.App.GetFullUrl(productLink)
-			if !exist {
-				ctx.App.Logger.Warn("Failed to find product link")
-				return fmt.Errorf("product link not found")
-			}
-			urls = append(urls, ninjacrawler.UrlCollection{Url: fullUrl, Parent: ctx.UrlCollection.Url})
-
-			return nil
-		})
-
-		if err != nil {
-			ctx.App.Logger.Warn("Error processing item: %v", err)
+		if _, err := ctx.Page.WaitForSelector(productLinkSelector); err != nil {
+			ctx.App.Logger.Warn("Failed to open modal, item index %d of %d %s %v", i, len(items), ctx.UrlCollection.Url, err)
 			closeModal(ctx)
 			continue
 		}
 
-		closeModal(ctx)
-
-		// Add a delay after every 50 items
-		if (i+1)%50 == 0 {
-			ctx.App.Logger.Info("Sleeping for 5 seconds...")
-			time.Sleep(5 * time.Second)
+		doc, err := ctx.App.GetPageDom(ctx.Page)
+		if err != nil {
+			ctx.App.Logger.Warn("Error getting page DOM:", err)
+			closeModal(ctx)
+			continue
 		}
+
+		productLink, exist := doc.Find(productLinkSelector).First().Attr("href")
+		if !exist {
+			ctx.App.Logger.Warn("Failed to find product link")
+			closeModal(ctx)
+			continue
+		}
+
+		fullUrl := ctx.App.GetFullUrl(productLink)
+		urls = append(urls, ninjacrawler.UrlCollection{Url: fullUrl, Parent: ctx.UrlCollection.Url})
+		closeModal(ctx)
 	}
+
 	return urls
 }
 
