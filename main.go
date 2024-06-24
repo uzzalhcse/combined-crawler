26a706cb92b1d9997e6cc6d3cb55ea2957c88309
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-06-24 18:31:01 +0600

    Comment out Kyocera, Aqua, and Sandvik handlers in main.go and update Markt handler configuration: set ConcurrentLimit to 1, disable dynamic processing, and comment out BoostCrawling.

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"combined-crawler/handlers/aqua"
-	"combined-crawler/handlers/kyocera"
 	"combined-crawler/handlers/markt"
-	"combined-crawler/handlers/sandvik"
 	"combined-crawler/pkg/ninjacrawler"
 )
 
@@ -12,9 +9,9 @@ func main() {
 
 	// register all sites configs to run the crawlers
 	ninjacrawler.NewNinjaCrawler().
-		AddSite(kyocera.Crawler()).
-		AddSite(aqua.Crawler()).
+		//AddSite(kyocera.Crawler()).
+		//AddSite(aqua.Crawler()).
 		AddSite(markt.Crawler()).
-		AddSite(sandvik.Crawler()).
+		//AddSite(sandvik.Crawler()).
 		Start()
 }
