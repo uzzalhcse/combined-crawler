be6d2941943af600a2555dd2ebbccf807712784f
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-06-28 10:37:44 +0600

    Update Yamaya handler configuration: disable BoostCrawling and adjust SleepAfter value in main.go

--- handlers/yamaya/main.go
+++ handlers/yamaya/main.go
@@ -7,11 +7,11 @@ func Crawler() ninjacrawler.CrawlerConfig {
 		Name: "yamaya1",
 		URL:  "https://drive.yamaya.jp/index.php",
 		Engine: ninjacrawler.Engine{
-			IsDynamic:       false,
-			BoostCrawling:   true,
+			IsDynamic: false,
+			//BoostCrawling:   true,
 			DevCrawlLimit:   2,
 			ConcurrentLimit: 5,
-			SleepAfter:      20,
+			SleepAfter:      10,
 		},
 		Handler: ninjacrawler.Handler{
 			UrlHandler:     UrlHandler,
