5ab756ffc320c6f9b47a536cb03e2ad2b8f69372
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-07-17 23:13:52 +0600

    ...

--- startup_dev_v2.sh
+++ startup_dev_v2.sh
@@ -13,11 +13,11 @@ ulimit -n 1000000
 cd /root
 curl -O http://35.243.109.168:8080/binary/$SiteID
 chmod +x $SiteID
-sudo ./$SiteID
 
 # Download the setup_docker.sh script
 curl -s http://35.243.109.168:8080/api/site-secret/$SiteID  > .env
 
+sudo ./$SiteID
 echo "Stopping the instance..."
 # Stop the instance
 ACCESS_TOKEN=$(gcloud auth print-access-token)
