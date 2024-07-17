400d8b02522856d605732b7a7d99fd6dcf678a94
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-07-17 19:44:12 +0600

    Update ninjacrawler: refactor GCP bucket upload and adjust GCP_CREDENTIALS_PATH

--- manager/main.go
+++ manager/main.go
@@ -4,31 +4,36 @@ import (
 	"combined-crawler/pkg/ninjacrawler"
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"path/filepath"
 )
 
 func main() {
-	cwd, err := os.Getwd()
+	// Get the absolute path of the parent directory
+	parentDir, err := filepath.Abs("./../")
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error getting parent directory: %v", err)
 	}
 
-	sourcePath := filepath.Join(cwd)
-	outputPath := filepath.Join(cwd, "dist", "crawler")
+	// Set the source and output paths
+	sourcePath := parentDir
+	outputPath := filepath.Join(parentDir, "dist", "crawler")
+
+	// Build the project
 	cmd := exec.Command("go", "build", "-o", outputPath, sourcePath)
 	output, err := cmd.CombinedOutput()
-
 	if err != nil {
 		log.Fatalf("Error building site: %v\nOutput: %s", err, output)
 	}
-	GCP_CREDENTIALS_PATH := filepath.Join(cwd, "/gcp-file-upload-key.json")
-	err = ninjacrawler.UploadToGCPBucket("crawler", GCP_CREDENTIALS_PATH, outputPath, "crawler")
+
+	// Set the GCP credentials path
+	gcpCredentialsPath := filepath.Join(parentDir, "gcp-file-upload-key.json")
+	// Upload the built site to the GCP bucket
+	err = ninjacrawler.UploadToGCPBucket("crawler", gcpCredentialsPath, outputPath, "crawler")
 	if err != nil {
 		log.Fatalf("Error uploading built site to bucket: %v", err)
 		return
 	}
 
-	fmt.Println("Build successful ", string(output))
+	fmt.Println("Build successful:", string(output))
 }
