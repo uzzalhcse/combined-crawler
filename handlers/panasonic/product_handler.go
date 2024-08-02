49a081c5f1f04b1d49a38a20616c8fdb7d6137a9
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-08-02 13:42:04 +0600

    implement PreHandlers

--- handlers/panasonic/product_handler.go
+++ handlers/panasonic/product_handler.go
@@ -3,6 +3,10 @@ package panasonic
 import (
 	"combined-crawler/constant"
 	"combined-crawler/pkg/ninjacrawler"
+	"fmt"
+	"net/url"
+	"path"
+	"path/filepath"
 )
 
 func ProductHandler(crawler *ninjacrawler.Crawler) {
@@ -45,7 +49,40 @@ func ProductHandler(crawler *ninjacrawler.Crawler) {
 			Entity:           constant.ProductDetails,
 			OriginCollection: constant.Products,
 			Processor:        productDetailSelector,
-			Preference:       ninjacrawler.Preference{ValidationRules: []string{"PageTitle"}},
+			Preference: ninjacrawler.Preference{
+				ValidationRules: []string{"PageTitle|required|blacklist:ページが見つかりません | Panasonic,エラー,URL変更のお知らせ,Redirect"},
+				PreHandlers: []func(c ninjacrawler.PreHandlerContext) error{
+					ValidHost,
+					HandleUrlExtension,
+				},
+			},
 		},
 	})
+}
+
+func ValidHost(c ninjacrawler.PreHandlerContext) error {
+	if !isValidHost(c.UrlCollection.Url) {
+		_ = c.App.MarkAsMaxErrorAttempt(c.UrlCollection.Url, constant.Products)
+		return fmt.Errorf("invalid host %s", c.UrlCollection.Url)
+	}
+	return nil
+}
+func HandleUrlExtension(c ninjacrawler.PreHandlerContext) error {
+	ext := GetUrlFileExtension(c.UrlCollection.Url)
+	if ext != "" && ext != ".html" && ext != ".htm" {
+		_ = c.App.MarkAsMaxErrorAttempt(c.UrlCollection.Url, constant.Products)
+		return fmt.Errorf("invalid Url Extension %s", c.UrlCollection.Url)
+	}
+	return nil
+}
+func GetUrlFileExtension(urlString string) string {
+	u, err := url.Parse(urlString)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return ""
+	}
+	fileName := path.Base(u.Path)
+	extension := filepath.Ext(fileName)
+
+	return extension
 }
