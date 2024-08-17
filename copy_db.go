f13267162960b37cda8c8c760cbc4b71ece7e067
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-08-17 10:22:30 +0600

    kojima

--- copy_db.go
+++ copy_db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -34,32 +35,53 @@ func main() {
 	sourceCollection := sourceDatabase.Collection("product_details")
 	destinationCollection := destinationDatabase.Collection("product_details")
 
-	// Find all documents in the source collection
-	cursor, err := sourceCollection.Find(context.TODO(), bson.M{})
+	// Define the batch size
+	batchSize := 1000
+
+	// Set up options for the cursor
+	findOptions := options.Find().SetBatchSize(int32(batchSize))
+
+	// Find all documents in the source collection with the specified options
+	cursor, err := sourceCollection.Find(context.TODO(), bson.M{}, findOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer cursor.Close(context.TODO())
 
 	var documents []interface{}
+	var totalCopied int
 
-	// Iterate over the cursor and decode each document
+	// Iterate over the cursor and decode each document in batches
 	for cursor.Next(context.TODO()) {
 		var document bson.M
 		if err := cursor.Decode(&document); err != nil {
 			log.Fatal(err)
 		}
 		documents = append(documents, document)
+
+		// When the batch is full, insert it into the destination collection
+		if len(documents) >= batchSize {
+			insertResult, err := destinationCollection.InsertMany(context.TODO(), documents)
+			if err != nil {
+				log.Fatal(err)
+			}
+			totalCopied += len(insertResult.InsertedIDs)
+			fmt.Printf("Copied %d documents so far...\n", totalCopied)
+			documents = nil // Clear the slice for the next batch
+
+			// Sleep to prevent overwhelming the server
+			time.Sleep(1 * time.Second)
+		}
 	}
 
-	// Insert all documents into the destination collection
+	// Insert any remaining documents that didn't fill a full batch
 	if len(documents) > 0 {
 		insertResult, err := destinationCollection.InsertMany(context.TODO(), documents)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Copied %d documents to the destination collection\n", len(insertResult.InsertedIDs))
-	} else {
-		fmt.Println("No documents found in the source collection.")
+		totalCopied += len(insertResult.InsertedIDs)
 	}
+
+	fmt.Printf("Copied %d documents to the destination collection\n", totalCopied)
 }
