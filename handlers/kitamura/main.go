6cacade68506375ae9f99f7583eb0d6ff95f5230
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-09-02 12:29:46 +0600

    kitamura

--- handlers/kitamura/main.go
+++ handlers/kitamura/main.go
@@ -7,18 +7,20 @@ func Crawler() ninjacrawler.CrawlerConfig {
 		Name: "kitamura",
 		URL:  "https://shop.kitamura.jp/",
 		Engine: ninjacrawler.Engine{
+			BrowserType:     "webkit",
 			IsDynamic:       ninjacrawler.Bool(true),
 			DevCrawlLimit:   999999,
 			ConcurrentLimit: 1,
 			//WaitForDynamicRendering: true,
-			BlockResources: true,
-			Provider:       "zenrows",
-			ProviderOption: ninjacrawler.ProviderQueryOption{
-				JsRender:       true,
-				CustomHeaders:  true,
-				OriginalStatus: true,
-				Wait:           5000,
-			},
+			WaitForSelector: ninjacrawler.String(".category-item"),
+			BlockResources:  true,
+			//Provider:       "zenrows",
+			//ProviderOption: ninjacrawler.ProviderQueryOption{
+			//	JsRender:       true,
+			//	CustomHeaders:  true,
+			//	OriginalStatus: true,
+			//	Wait:           5000,
+			//},
 		},
 		Handler: ninjacrawler.Handler{
 			UrlHandler: UrlHandler,
