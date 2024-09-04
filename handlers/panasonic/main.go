2cf4b6ed4380efa710f42d1b98f27cebb0b1fdbd
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-09-04 20:25:41 +0600

    implement handleThrottling and retry

--- handlers/panasonic/main.go
+++ handlers/panasonic/main.go
@@ -9,12 +9,12 @@ func Crawler() ninjacrawler.CrawlerConfig {
 		Name: "panasonic",
 		URL:  "https://panasonic.jp/products.html",
 		Engine: ninjacrawler.Engine{
-			IsDynamic:       ninjacrawler.Bool(false),
-			DevCrawlLimit:   200,
-			ConcurrentLimit: 10,
-			SleepAfter:      10,
-			Timeout:         30,
-			BoostCrawling:   true,
+			IsDynamic:          ninjacrawler.Bool(false),
+			DevCrawlLimit:      200,
+			ConcurrentLimit:    10,
+			SleepAfter:         15,
+			Timeout:            30,
+			RetrySleepDuration: 15,
 		},
 		Handler: ninjacrawler.Handler{
 			UrlHandler:     UrlHandler,
