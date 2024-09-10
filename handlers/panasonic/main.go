1a9881133d066b03f85070baa1e872d9cc06a397
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-09-10 15:17:55 +0600

    zenrows implement with no sleep

--- handlers/panasonic/main.go
+++ handlers/panasonic/main.go
@@ -11,7 +11,7 @@ func Crawler() ninjacrawler.CrawlerConfig {
 		Engine: ninjacrawler.Engine{
 			IsDynamic:       ninjacrawler.Bool(false),
 			DevCrawlLimit:   200,
-			ConcurrentLimit: 10,
+			ConcurrentLimit: 15,
 			SleepAfter:      5,
 			Timeout:         3600,
 			//RetrySleepDuration: 30,
