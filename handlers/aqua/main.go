30f2d5a04b6f10a458163a25373d003716752970
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-09-19 18:41:23 +0600

    snowpeak test

--- handlers/aqua/main.go
+++ handlers/aqua/main.go
@@ -7,10 +7,11 @@ func Crawler() ninjacrawler.CrawlerConfig {
 		Name: "snowpeak",
 		URL:  "https://ec.snowpeak.co.jp/snowpeak/ja/%E3%82%AD%E3%83%A3%E3%83%B3%E3%83%97/c/2010000",
 		Engine: ninjacrawler.Engine{
-			IsDynamic:       ninjacrawler.Bool(true),
-			DevCrawlLimit:   300,
-			ConcurrentLimit: 1,
-			ProxyStrategy:   ninjacrawler.ProxyStrategyRotation,
+			IsDynamic:              ninjacrawler.Bool(true),
+			DevCrawlLimit:          300,
+			ConcurrentLimit:        1,
+			ForceInstallPlaywright: true,
+			ProxyStrategy:          ninjacrawler.ProxyStrategyRotation,
 			ProxyServers: []ninjacrawler.Proxy{
 				{
 					Server:   "http://5.59.251.78:6117",
