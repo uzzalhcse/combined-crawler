404726c1283f3e92c6a0a9b1de7a05abb4cde1fe
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-10-15 09:48:44 +0600

    pkg fix

--- pkg/ninjacrawler/processor.go
+++ pkg/ninjacrawler/processor.go
@@ -139,11 +139,13 @@ func (app *Crawler) assignProxy(proxy Proxy, proxyLock *sync.Mutex) {
 		app.Logger.Fatal("No proxies available")
 		return
 	}
-	proxyLock.Lock()
-	app.CurrentProxy = proxy
-	atomic.StoreInt32(&app.CurrentProxyIndex, atomic.LoadInt32(&app.CurrentProxyIndex)%int32(len(app.engine.ProxyServers)))
+	if len(app.engine.ProxyServers) > 0 {
+		proxyLock.Lock()
+		app.CurrentProxy = proxy
+		atomic.StoreInt32(&app.CurrentProxyIndex, atomic.LoadInt32(&app.CurrentProxyIndex)%int32(len(app.engine.ProxyServers)))
 
-	proxyLock.Unlock()
+		proxyLock.Unlock()
+	}
 }
 
 func (app *Crawler) crawlWithProxies(urlCollection UrlCollection, config ProcessorConfig, attempt int) bool {
